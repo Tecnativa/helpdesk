594ff92468089ebc68eb71baff5fe4f796cf1833
sergio-teruel <sergio.teruel@tecnativa.com>
2022-09-20 12:29:57 +0200

    [ADD] sale_order_general_discount_sale_pwa_oca: new module TT39073

---
+++ sale_order_general_discount_sale_pwa_oca/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Tecnativa - Alexandre D. Díaz
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Order General Discount Set Sale PWA OCA",
+    "summary": "Adds support for sale order general discount set in the standalone mode",
+    "version": "13.0.1.0.0",
+    "category": "Sale",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://gitlab.tecnativa.com/extra-addons",
+    "license": "AGPL-3",
+    "depends": ["sale_pwa_oca", "sale_order_general_discount"],
+    "data": ["views/sale_pwa_views.xml"],
+    "installable": True,
+    "auto_install": False,
+}
