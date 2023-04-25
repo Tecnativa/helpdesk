28f7a6c83b8901e67f1dd2812d40f1daa2e6b8fb
sergio-teruel <sergio.teruel@tecnativa.com>
2023-04-25 16:43:34 +0200

    [FIX] stock_picking_batch_report: Remove product_fishing dependency

--- stock_picking_batch_report/__manifest__.py
+++ stock_picking_batch_report/__manifest__.py
@@ -18,7 +18,6 @@
         "product_weight_type",
         "stock_location_type_good",
         "stock_secondary_unit",
-        "product_fishing",
     ],
     "data": [
         "data/report_paperformat.xml",
