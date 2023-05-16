b627aa072b875ff223a7b5301f5c372ad4c5f92b
sergio-teruel <sergio.teruel@tecnativa.com>
2023-05-16 17:25:15 +0200

    [IMP] stock_production_lot_fishing_label: Filter origin moves to create print lines

--- stock_production_lot_fishing_label/wizards/stock_barcode_selection_printing.py
+++ stock_production_lot_fishing_label/wizards/stock_barcode_selection_printing.py
@@ -1,15 +1,7 @@
 # Copyright 2023 Tecnativa - Carlos Roca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
-
-
-class ProductPrintingQty(models.TransientModel):
-    _inherit = "stock.picking.line.print"
-
-    result_package_id = fields.Many2one(
-        comodel_name="stock.quant.package", related="move_line_id.result_package_id"
-    )
+from odoo import api, models
 
 
 class WizStockBarcodeSelectionPrinting(models.TransientModel):
@@ -17,14 +9,15 @@ class WizStockBarcodeSelectionPrinting(models.TransientModel):
 
     @api.model
     def _get_move_lines(self, picking):
+        stock_move_lines = super()._get_move_lines(picking)
         if self.barcode_report == self.env.ref(
             "stock_production_lot_fishing_label.action_label_fishing_report"
         ):
-            return picking.move_line_ids.filtered(
+            return stock_move_lines.filtered(
                 lambda ml: ml.product_id.default_code and ml.lot_id
             )
         elif self.barcode_report == self.env.ref(
             "stock_production_lot_fishing_label.action_label_fishing_report_mini"
         ):
-            return picking.move_line_ids.filtered("result_package_id")
-        return super()._get_move_lines(picking)
+            return stock_move_lines.filtered("result_package_id")
+        return stock_move_lines
