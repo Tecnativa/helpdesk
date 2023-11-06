5732837fbd9c72b58bb4e71bba1073b5cbf87445
sergio-teruel <sergio.teruel@tecnativa.com>
2023-11-06 21:09:57 +0100

    [FIX] stock_barcodes_gs1_generator: Adapt mixin

--- stock_barcodes_gs1_generator/models/barcode_gs1_label_mixin.py
+++ stock_barcodes_gs1_generator/models/barcode_gs1_label_mixin.py
@@ -13,7 +13,26 @@ class BarcodeGs1LabelMixin(models.AbstractModel):
     barcode = fields.Char(compute="_compute_barcode")
     barcode_human_readable = fields.Char(compute="_compute_barcode_human_readable")
 
-    @api.depends(lambda s: ["product_id", "lot_id"] + s._qty_field)
+    @api.model
+    def _get_field_gs1_depends(self):
+        field_list = [
+            "product_id",
+        ]
+        if self._name != "stock.production.lot":
+            field_list.append("lot_id")
+        if self._qty_field:
+            field_list.append(self._qty_field)
+        return field_list
+
+    def _get_lot_record(self):
+        # Is the record a lot?
+        if self._name == "stock.production.lot":
+            lot = self
+        else:
+            lot = self.lot_id
+        return lot
+
+    @api.depends(lambda s: s._get_field_gs1_depends())
     def _compute_barcode(self):
         for record in self:
             pattern = ""
@@ -21,16 +40,18 @@ class BarcodeGs1LabelMixin(models.AbstractModel):
                 record.barcode = ""
                 continue
             pattern += f"01{record.product_id.barcode}"
-            pattern += f"3103{str(int(record[self._qty_field[0]] * 1000)).zfill(6)}"
-            if record.lot_id:
-                if record.lot_id.expiration_date:
+            if self._qty_field:
+                pattern += f"3103{str(int(record[self._qty_field[0]] * 1000)).zfill(6)}"
+            lot = self._get_lot_record()
+            if lot:
+                if lot.expiration_date:
                     pattern += "15{}".format(
-                        format_date(record.lot_id.expiration_date, format="YYMdd")
+                        format_date(lot.expiration_date, format="YYMdd")
                     )
-                pattern += f"10{record.lot_id.name}"
+                pattern += f"10{lot.name}"
             record.barcode = pattern
 
-    @api.depends(lambda s: ["product_id", "lot_id"] + s._qty_field)
+    @api.depends(lambda s: s._get_field_gs1_depends())
     def _compute_barcode_human_readable(self):
         for record in self:
             pattern = ""
@@ -38,11 +59,15 @@ class BarcodeGs1LabelMixin(models.AbstractModel):
                 record.barcode_human_readable = ""
                 continue
             pattern += f"(01){record.product_id.barcode}"
-            pattern += f"(3103){str(int(record[self._qty_field[0]] * 1000)).zfill(6)}"
-            if record.lot_id:
-                if record.lot_id.expiration_date:
+            if self._qty_field:
+                pattern += (
+                    f"(3103){str(int(record[self._qty_field[0]] * 1000)).zfill(6)}"
+                )
+            lot = self._get_lot_record()
+            if lot:
+                if lot.expiration_date:
                     pattern += "(15){}".format(
-                        format_date(record.lot_id.expiration_date, format="YYMdd")
+                        format_date(lot.expiration_date, format="YYMdd")
                     )
-                pattern += f"(10){record.lot_id.name}"
+                pattern += f"(10){lot.name}"
             record.barcode_human_readable = pattern
